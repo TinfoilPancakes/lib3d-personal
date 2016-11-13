86a105c23186aac9e9fb237232f66e536ecbe5f1
Prateek R Patil <ppatil@student.42.us.org>
2016-11-12 17:15:28 -0800

    Fixed Line drawing. Now works in every direction! * Ugh. [ Let's Bounce ]

--- src/pixel_line_draw.c
+++ src/pixel_line_draw.c
@@ -12,6 +12,13 @@
 
 #include "pixel.h"
 
+static int	absi(int i)
+{
+	if (i < 0)
+		return (-i);
+	return (i);
+}
+
 void	pixel_line_draw(
 	t_pixel p0,
 	t_pixel p1,
@@ -22,25 +29,25 @@ void	pixel_line_draw(
 	t_pixel	delta;
 
 	delta = pixel_init(color(), p1.x - p0.x, p1.y - p0.y);
-	if (delta.x < delta.y)
+	if (absi(delta.x) < absi(delta.y))
 	{
 		delta = pixel_init(delta.c, delta.y, delta.x);
 		delta.c.b = 1;
 	}
 	s = vec2f_init(-1, (float)delta.y / delta.x);
-	while (delta.x)
+	while (delta.x != 0)
 	{
-		s.x += s.y;
+		s.x += (s.y < 0) ? -s.y : s.y;
 		if (s.x >= 0)
 		{
 			s.x -= 1;
-			p0.y++;
+			p0.y += (delta.y < 0) ? -1 : 1;
 		}
 		if (delta.c.b)
 			plot(pixel_init(p0.c, p0.y, p0.x), &p0.c, args);
 		else
 			plot(p0, &p0.c, args);
-		p0.x++;
-		delta.x--;
+		p0.x += (delta.x < 0) ? -1 : 1;
+		delta.x += (delta.x < 0) ? 1 : -1;
 	}
 }
